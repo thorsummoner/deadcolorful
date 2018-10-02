52f86b50ad69659dd937b6ac06ecababb3364d4a
Dylan Grafmyre <thorsummoner@live.com>
2018-10-01 23:20:03 -0700

    basic pygtk3 window

--- deadcolor/__main__.py
+++ deadcolor/__main__.py
@@ -3,17 +3,42 @@
     deadcolorful  simple dead pixel check
 """
 import argparse
+import signal
+
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
 
 ARGP = argparse.ArgumentParser(
     description=__doc__,
     formatter_class=argparse.RawTextHelpFormatter,
 )
 
+
+class DeadColorful():
+    def __init__(self):
+        self.window = Gtk.Window(
+            title=self.__class__.__name__t,
+        )
+        self.window.connect('destroy', self.on_destroy)
+
+    def main(self):
+        self.window.show_all()
+        Gtk.main()
+
+    def on_destroy(self, *args, **kwargs):
+        Gtk.main_quit()
+
+
 def main(argp=None):
     if argp is None:
         argp = ARGP.parse_args()  # pragma: no cover
 
-    print(argp)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    instance = DeadColorful()
+    instance.main()
 
 if __name__ == '__main__':
     main()  # pragma: no cover
